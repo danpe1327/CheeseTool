d38ffa5224d8b3c718f2028b114f7446169a48be
Operator <danpechen@126.com>
2019-08-06 10:53:28 +0800

    Add files via upload

---
+++ watermark/README.md
@@ -0,0 +1,136 @@
+## python 批量给 word 或 pdf 文件添加水印
+
+### 1.依赖库
+```
+# win32com，用于调用 word 等应用程序
+pip install pypiwin32
+
+# reportlab，用于生成 pdf 水印文件
+pip install reportlab
+
+# PyPDF4，用于合成 pdf 文件
+pip install pypdf4
+```
+
+### 2.修改pypdf4 的 utils.py，以支持中英文合成
+代码路径: \PyPDF4\utils.py
+
+将其中的 r = s.encode('latin-1') ，改为如下
+```
+    try:
+        r = s.encode('latin-1')            
+        if len(s) < 2:
+            bc[s] = r
+        return r
+    except Exception as e:
+        r = s.encode('utf-8')
+        if len(s) < 2:
+            bc[s] = r
+        return r
+```
+
+### 3.word 转换为 pdf 格式
+通过 win32com.client 调用 Word 应用程序，打开 word 文件并另存为 pdf 格式。
+```
+def word2pdf(word_file):
+    doc_ext = os.path.splitext(os.path.basename(word_file))[1].lower()
+    pdf_file = word_file.replace(doc_ext, '.pdf')
+
+    try:
+        word = client.DispatchEx("Word.Application")
+
+        if os.path.exists(pdf_file):
+            os.remove(pdf_file)
+        worddoc = word.Documents.Open(word_file, ReadOnly=1)
+        worddoc.SaveAs(pdf_file, FileFormat=17)  # 保存文件为 pdf 格式
+        worddoc.Close()
+        word.Quit()
+        return pdf_file
+    except Exception as e:
+        print(e)
+        return None
+```
+
+### 4.创建水印文件
+水印的位置与角度需要手动去调，以满足需求。还可以设置字体，字体的大小、颜色、透明度等参数。
+```
+def create_watermark(content, angle, font_size=36):
+    """
+    创建 PDF 水印模板
+    """
+
+    wm_file = 'watermark.pdf'
+    pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))  # 加载字体
+    c = canvas.Canvas(wm_file)  # 使用reportlab来创建一个PDF文件来作为一个水印文件
+    
+    # 配置水印文件
+    c.setFont('Arial', font_size)
+    c.saveState()
+    c.rotate(angle)
+    c.setFillAlpha(0.2)
+
+    content_lst = content.split(' ')
+    y = 0
+    for c_item in content_lst:
+        c.drawString(200, 100 - y, c_item)
+        c.drawString(450, 350 - y, c_item)
+        c.drawString(750, 250 - y, c_item)
+        c.drawString(550, 0 - y, c_item)
+        y += font_size + 4
+
+    c.restoreState()
+    c.save()
+
+    return wm_file
+```
+
+### 5.合并水印文件与资料文件
+通过 PdfFileReader 读取 pdf 文件，并使用 PdfFileWriter 新建一个空的 pdf，merge 水印文件，并保存。原始的 pypdf4 不支持中文的编码，需要修改 utils.py 文件（见 2.）。
+```
+def add_watermark(pdf_file, wm_file):
+    wm_obj = PdfFileReader(wm_file)
+    wm_page = wm_obj.getPage(0)
+
+    out_file = os.path.join(os.path.dirname(pdf_file), 'wm_%s' % os.path.basename(pdf_file))
+    pdf_reader = PdfFileReader(pdf_file)
+    pdf_writer = PdfFileWriter()
+
+    for page_num in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(page_num)
+        page.mergePage(wm_page)
+        pdf_writer.addPage(page)
+
+    with open(out_file, 'wb') as out:
+        pdf_writer.write(out)
+```
+
+### 6.使用说明
+命令
+```
+python add_watermark.py input_file 
+                        --watermark DANPE
+                        --angle 45
+                        --font_file arial.ttf                        
+                        --font_size 36
+                        --color black
+                        --alpha 0.2
+
+# 参数说明 input_file 输入文件路径，目前支持 word， excel， powerpoint 的新旧 6 种格式与 pdf 格式
+           --watermark 水印文本，通过符号 ‘|’ 换行
+           --angle 水印文本方向
+           --font_file 可自定义字体文件，若无输入或字体文件不存在，则使用默认的字体
+           --font_size 字体大小
+           --color 水印颜色，可选常见的颜色，如 [black, red, blue, green, yellow, white, gold, purple, pink,         orange] 等
+           --alpha 字体透明度
+           --only_pdf 只转换文本为 pdf，不添加水印
+           --no_date 水印不加入日期
+```
+
+### 7.常见错误
+- 转换 ppt 文件时，出现错误 “The Python instance can not be converted to a COM object”
+  
+  在保存成 pdf 文件时，需要输入参数 PrintRange
+  ```
+  office_file.ExportAsFixedFormat(pdf_file, 32, PrintRange=None)
+  ```
+  
---
+++ watermark/add_watermark.py
@@ -0,0 +1,273 @@
+import os
+import time
+import argparse
+import shutil
+from tqdm import tqdm
+from win32com import client
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib import colors
+from PyPDF4 import PdfFileReader, PdfFileWriter
+
+
+class PdfConvert(object):
+
+    def run_convert(self, in_file, save_dir):
+        file_ext = os.path.splitext(os.path.basename(in_file))[1].lower()
+        pdf_file = in_file.replace(file_ext, '.pdf')
+        pdf_file = os.path.join(save_dir, os.path.basename(pdf_file))
+
+        if file_ext in ['.doc', '.docx']:
+            out_file = self.word2pdf(src_file, pdf_file)
+        elif file_ext in ['.ppt', '.pptx']:
+            out_file = self.ppt2pdf(src_file, pdf_file)
+        elif file_ext in ['.xls', '.xlsx']:
+            out_file = self.excel2pdf(src_file, pdf_file)
+        else:
+            return None
+
+        if isinstance(out_file, str):
+            pdf_lst = [out_file]
+        else:
+            pdf_lst = out_file
+
+        return pdf_lst
+
+    def word2pdf(self, in_file, pdf_file):
+        try:
+            office_app = client.DispatchEx("Word.Application")
+
+            if os.path.exists(pdf_file):
+                os.remove(pdf_file)
+
+            office_file = office_app.Documents.Open(in_file, Visible=False, ReadOnly=1)
+            office_file.ExportAsFixedFormat(pdf_file, 17)
+            office_file.Close()
+            office_app.Quit()
+            return pdf_file
+        except Exception as e:
+            print('failed to convert word %s' % src_file, e)
+            office_app.Quit()
+            return None
+
+    def excel2pdf(self, in_file, pdf_file):
+        try:
+            pdf_lst = list()
+            office_app = client.DispatchEx("Excel.Application")
+
+            office_file = office_app.Workbooks.Open(in_file, ReadOnly=1)
+            sheet_num = office_file.Sheets.Count
+
+            # 循环保存每一个sheet
+            for i in range(1, sheet_num + 1):
+                sheet_name = office_file.Sheets(i).Name
+                xls_sheet = office_file.Worksheets(sheet_name)
+
+                if str(xls_sheet.UsedRange) == 'None':  # 过滤没有内容的sheet
+                    continue
+
+                pdf_file = pdf_file.replace('.pdf', '_%s.pdf' % sheet_name)
+
+                if os.path.exists(pdf_file):
+                    os.remove(pdf_file)
+
+                xls_sheet.ExportAsFixedFormat(0, pdf_file)
+                pdf_lst.append(pdf_file)
+
+            office_file.Close()
+            office_app.Quit()
+            return pdf_lst
+        except Exception as e:
+            print('failed to convert excel %s' % src_file, e)
+            office_app.Quit()
+            return None
+
+    def ppt2pdf(self, in_file, pdf_file):
+        try:
+            office_app = client.DispatchEx("Powerpoint.Application")
+
+            if os.path.exists(pdf_file):
+                os.remove(pdf_file)
+
+            office_file = office_app.Presentations.Open(in_file, WithWindow=False, ReadOnly=1)
+            office_file.ExportAsFixedFormat(pdf_file, 32, PrintRange=None)
+            office_file.Close()
+            office_app.Quit()
+            return pdf_file
+        except Exception as e:
+            print('failed to convert ppt %s' % src_file, e)
+            office_app.Quit()
+            return None
+
+
+def create_watermark(content, angle, direction='v', font_file=None, font_size=36, color='black', alpha=0.2):
+    """
+    创建 PDF 水印模板
+    """
+
+    wm_file = 'watermark_%s.pdf' % direction
+    if font_file is None or not os.path.exists(font_file):
+        available_fonts = pdfmetrics.getRegisteredFontNames()
+        font_name = available_fonts[0]
+    else:
+        font_name = os.path.splitext(os.path.basename(font_file))[0]
+        pdfmetrics.registerFont(TTFont(font_name, font_file))  # 加载字体
+
+    c = canvas.Canvas(wm_file)  # 使用reportlab来创建一个PDF文件来作为一个水印文件
+
+    # 配置水印文件
+    c.setFont(font_name, font_size)
+    c.saveState()
+    c.rotate(angle)
+    c.setFillColor(eval('colors.%s' % color))
+    c.setFillAlpha(alpha)
+
+    content_lst = content.split('|')
+    y = 0
+    if direction == 'v':
+        for c_item in content_lst:
+            c.drawString(200, 100 - y, c_item)
+            c.drawString(450, 350 - y, c_item)
+            c.drawString(750, 250 - y, c_item)
+            c.drawString(550, 0 - y, c_item)
+            y += font_size + 4
+    else:
+        for c_item in content_lst:
+            c.drawString(200, 20 - y, c_item)
+            c.drawString(500, 60 - y, c_item)
+            c.drawString(750, -200 - y, c_item)
+            c.drawString(400, -300 - y, c_item)
+            y += font_size + 4
+    c.restoreState()
+    c.save()
+
+    return wm_file
+
+
+def add_watermark(pdf_file, save_dir, wm_file_v, wm_file_h):
+    out_file = os.path.join(save_dir, os.path.basename(pdf_file))
+
+    pdf_reader = PdfFileReader(pdf_file)
+    pdf_writer = PdfFileWriter()
+
+    wm_obj_v = PdfFileReader(wm_file_v)
+    wm_page_v = wm_obj_v.getPage(0)
+    wm_obj_h = PdfFileReader(wm_file_h)
+    wm_page_h = wm_obj_h.getPage(0)
+
+    for page_num in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(page_num)
+        current_page = pdf_reader.getPage(page_num)
+        width = current_page.mediaBox.getWidth()
+        height = current_page.mediaBox.getHeight()
+        wm_page = wm_page_v if height >= width else wm_page_h
+        page.mergePage(wm_page)
+        pdf_writer.addPage(page)
+
+    with open(out_file, 'wb') as out:
+        pdf_writer.write(out)
+
+
+def listFiles(dir, out_list, types, recursion=False):
+    """
+    dir --string
+        文件目录
+    out_list --list
+        文件列表
+    types --list[str]
+        查找的文件类型
+    recursion --bool
+        是否递归
+    """
+
+    files = os.listdir(dir)
+    for name in files:
+        fullname = os.path.join(dir, name)
+        if os.path.isdir(fullname):
+            if recursion:
+                listFiles(fullname, out_list, types, recursion)
+        else:
+            _, ext = os.path.splitext(name)
+            if ext != '' and ext.lower() in types:
+                out_list.append(fullname)
+    return out_list
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', type=str, help='input docx or pdf file or directory')
+    parser.add_argument('--watermark', type=str, help='Wrap through |', default='DANPE|')
+    parser.add_argument('--angle', type=int, help='', default=45)
+    parser.add_argument('--font_file', type=str, help='', default='arial.ttf')
+    parser.add_argument('--font_size', type=int, help='', default=36)
+    parser.add_argument('--color', type=str, help='', default='black')
+    parser.add_argument('--alpha', type=float, help='', default=0.2)
+    parser.add_argument('--only_pdf', action='store_true', help='', default=False)
+    parser.add_argument('--no_date', action='store_true', help='the watermark with no date information', default=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    OFFICE_PDF_EXT = ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf']
+    args = parse_args()
+    input_file = os.path.abspath(args.input_file)
+
+    wm_content = args.watermark
+    if not args.no_date:
+        date_str = time.strftime('%Y.%m.%d')
+        wm_content += date_str
+
+    word_file_lst = list()
+    if os.path.isdir(input_file):
+        listFiles(input_file, word_file_lst, OFFICE_PDF_EXT, True)
+        out_dir = os.path.join(input_file, '..', '%s-with-watermark' % os.path.basename(input_file))
+    else:
+        word_file_lst = [input_file]
+        out_dir = os.path.join(os.getcwd(), 'with-watermark')
+
+    wm_file_v = create_watermark(wm_content, args.angle, 'v', args.font_file, args.font_size, args.color,
+                                 args.alpha)  # for Portrait
+    wm_file_h = create_watermark(wm_content, args.angle, 'h', args.font_file, args.font_size, args.color,
+                                 args.alpha)  # for Landscape
+    pdf_convert = PdfConvert()
+    for src_file in tqdm(word_file_lst):
+        src_file = os.path.normpath(src_file)
+        if os.path.basename(src_file).startswith(tuple(('wm_', '~'))) or 'with-watermark' in src_file:
+            print('illegal file %s' % src_file)
+            continue
+
+        save_dir = out_dir
+        if os.path.isdir(input_file):
+            sub_dir = os.path.dirname(src_file)
+            save_dir = os.path.join(out_dir, sub_dir.split(input_file)[1][1:])
+
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        # print('add watermark for %s' % src_file)
+
+        file_ext = os.path.splitext(os.path.basename(src_file))[1].lower()
+        if file_ext == '.pdf':
+            pdf_file = os.path.join(save_dir, os.path.basename(src_file))
+            shutil.copy(src_file, pdf_file)
+            pdf_lst = [pdf_file]
+        else:
+            pdf_lst = pdf_convert.run_convert(src_file, save_dir)
+
+        if not args.only_pdf and pdf_lst is not None:
+            try:
+                for pdf_item in pdf_lst:
+                    add_watermark(pdf_item, save_dir, wm_file_v, wm_file_h)  # 添加水印，并覆盖 pdf 文件
+
+            except Exception as e:
+                print('failed to add watermark %s' % src_file, e)
+                for pdf_item in pdf_lst:
+                    os.remove(pdf_item)
+                continue
+
+    os.remove(wm_file_v)
+    os.remove(wm_file_h)
